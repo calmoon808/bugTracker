052477b82e229a44f8540768d2f47b68e68e9555
Calvin <calmoon808@gmail.com>
2020-02-26 00:01:23 -1000

    base layout for auth page

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/pages/AuthorizationPage/AutorizationPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AutorizationPage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default AutorizationPage;
