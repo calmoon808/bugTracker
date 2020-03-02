cdbb4a767928433da3584687164e47cd361f866a
Calvin <calmoon808@gmail.com>
2020-03-01 17:18:27 -1000

    added toggle option for dashboard component and added a redirect to login page

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import "semantic-ui-react";
 import { Container } from "semantic-ui-react";
 import NavigationMenu from "../components/NavigatrionMenuComponent/NavigationMenu";
@@ -8,21 +8,30 @@ import AuthorizationPage from '../pages/AuthorizationPage';
 import HomePage from '../pages/HomePage';
 import DashboardComponent from '../components/DashboardComponent';
 
-export default function App() {
-  return (
-    <Router>
-      <NavigationMenu />
-      <DashboardComponent />
-      <Container className="Container">
-        <Switch>
-          <Route path="/authorization">
-           <AuthorizationPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </Container>
-    </Router>
-  )
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoggedIn: false
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <NavigationMenu />
+        {this.state.isLoggedIn ? <DashboardComponent /> : <Redirect to="/authorization" />}
+        <Container className="Container">
+          <Switch>
+            <Route path="/authorization">
+             <AuthorizationPage />
+            </Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
+    )
+  }
 }
