a78bbeb1b2424e36970a73d52fc8465f00deedd1
Calvin <calmoon808@gmail.com>
2020-03-01 15:10:55 -1000

    added bug icon

--- src/components/NavigatrionMenuComponent/NavigationMenu.jsx
+++ src/components/NavigatrionMenuComponent/NavigationMenu.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Container, Image, Menu } from 'semantic-ui-react';
 import styles from "./NavigationMenu.module.scss";
+import bugIcon from "../../icons/bugIcon.png"
 
 export default () => (
   <Menu className={styles.Menu}>
     <Container>
       <Menu.Item as="a" header position="left">
         <Image
-          size="small"
-          src="/img/page/logo.svg"
+          size="mini"
+          src={bugIcon}
         />
       </Menu.Item>
       <Menu.Menu position="right">
