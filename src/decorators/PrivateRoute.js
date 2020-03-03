36e1c8fbbcd3860da7651a002e0bb65c597798e5
Calvin <calmoon808@gmail.com>
2020-03-02 22:05:29 -1000

    added decorators for private routes

---
+++ src/decorators/PrivateRoute.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth } from "../context/auth";
+
+export default function PrivateRoute({ component: Component, ...rest }) { 
+  const isAuthenticated = useAuth();
+
+  return (
+    <Route {...rest} render={(props) => isAuthenticated ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to="/login" />
+    )}/>
+  );
+} 
