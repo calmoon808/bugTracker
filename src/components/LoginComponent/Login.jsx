b43d1867fd6a381d86660577bc81be45035a2ef4
Calvin <calmoon808@gmail.com>
2020-03-03 01:06:19 -1000

    added initial function setup for login component. Need to do same for sign up component, and make a post route to user database to test.

--- src/components/LoginComponent/Login.jsx
+++ src/components/LoginComponent/Login.jsx
@@ -1,48 +1,81 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import styles from "./Login.module.scss"
-import {
-  Button,
-  Form,
-  Grid,
-  Header,
-  Message,
-  Segment,
-} from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Link, Redirect } from "react-router-dom";
+import styles from "./Login.module.scss";
+import { useAuth } from "../../context/auth";
+import { Button, Form, Grid, Header, Message, Segment} from 'semantic-ui-react';
+import axios from "axios";
 
-export default () => (
-  <div className={styles.Login}>
-    <Grid centered columns={2}>
-      <Grid.Column>
-        <Header as="h2" textAlign="center">
-          Login
-        </Header>
-        <Segment>
-          <Form size="large">
-            <Form.Input
-              fluid
-              icon="envelope"
-              iconPosition="left"
-              placeholder="Email address"
-            />
-            <Form.Input
-              fluid
-              icon="lock"
-              iconPosition="left"
-              placeholder="Password"
-              type="password"
-            />
+const Login = () => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const { setAuthTokens } = useAuth();
 
-            <Button color="blue" fluid size="large">
-              Login
-            </Button>
-          </Form>
-        </Segment>
-        <Message>
-          Not registered yet? <Link to="/signup">Sign Up</Link>
-        </Message>
-      </Grid.Column>
-    </Grid>
-  </div>
-);
+  const postLogin = () => {
+    axios.post("https://www.somePlace.com/auth/login", {
+      userName,
+      password
+    }).then(result => {
+      if (result.status === 200) {
+        setAuthTokens(result.data);
+        setLoggedIn(true);
+      } else {
+        setIsError(true);
+      }
+    });
+  }
 
+  if (isLoggedIn) {
+    return <Redirect to="/" />
+  }
+
+  return(
+    <div className={styles.Login}>
+      <Grid centered columns={2}>
+        <Grid.Column>
+          <Header as="h2" textAlign="center">
+            Login
+          </Header>
+          <Segment>
+            <Form size="large">
+              <Form.Input
+                fluid
+                icon="envelope"
+                iconPosition="left"
+                placeholder="Email address"
+                onChange={e => {
+                  setUserName(e.target.value)
+                }}
+              />
+              <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+                onChange={e => {
+                  setPassword(e.target.value)
+                }}
+              />
+
+              <Button 
+                color="blue" 
+                fluid size="large"
+                onClick={postLogin}
+              >
+                Login
+              </Button>
+            </Form>
+          </Segment>
+          <Message>
+            Not registered yet? <Link to="/signup">Sign Up</Link>
+            { isError &&<div>The username or password provided were incorrect!</div> }
+          </Message>
+        </Grid.Column>
+      </Grid>
+    </div>
+  )
+};
+
+export default Login;
