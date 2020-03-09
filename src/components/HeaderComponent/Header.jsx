13be8f396b9404517f0c50e416c57da88db9a800
Calvin <calmoon808@gmail.com>
2020-03-08 15:16:24 -1000

    added links to header

--- src/components/HeaderComponent/Header.jsx
+++ src/components/HeaderComponent/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Image, Menu } from 'semantic-ui-react';
 import styles from "./Header.module.scss";
 import bugIcon from "../../icons/bugIcon.png"
@@ -13,11 +14,11 @@ export default () => (
         />
       </Menu.Item>
       <Menu.Menu position="right">
-        <Menu.Item as="a" name="login">
+        <Menu.Item as={Link} to="/login" name="login">
           Login
         </Menu.Item>
-        <Menu.Item as="a" name="register">
-          Register
+        <Menu.Item as={Link} to ="/signup" name="signup">
+          Sign Up
         </Menu.Item>
       </Menu.Menu>
     </Container>
