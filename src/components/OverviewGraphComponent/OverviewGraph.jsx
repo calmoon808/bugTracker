c18c4970f9655811f27e2f25aa835641fe980eea
Calvin <calmoon808@gmail.com>
2020-04-12 02:04:00 -1000

    basic doughnut map inserted

--- src/components/OverviewGraphComponent/OverviewGraph.jsx
+++ src/components/OverviewGraphComponent/OverviewGraph.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Card } from 'semantic-ui-react';
+import Chart from "chart.js";
 
 const OverviewGraph = (props) => {
+  let chartRef = React.createRef();
+  useEffect(() => {
+    const myChartRef = chartRef.current.getContext("2d");
+    new Chart(myChartRef, {
+      type: "doughnut",
+      data: {
+        labels: ["Open", "Closed"],
+        datasets: [
+          {
+            label: "Bugs",
+            data: [2, 8],
+            backgroundColor: ['green', 'red']
+          }
+        ]
+      },
+      options: {
+        // hover: {
+        //   // Overrides the global setting
+        //   mode: 'index'
+        // }
+        animation: {
+          animateRotate: true
+        }
+      }
+    })
+  })
 
   return (
     <Card>
@@ -9,6 +36,10 @@ const OverviewGraph = (props) => {
         <Card.Header>My Overview</Card.Header>
       </Card.Content>
       <Card.Content>
+        <canvas 
+          id="myChart"
+          ref={chartRef}  
+        />
       </Card.Content>
     </Card>
   );
