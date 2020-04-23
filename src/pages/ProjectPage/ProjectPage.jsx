20b54e256891d61275177e0914b44f0d9c20572f
Calvin <calmoon808@gmail.com>
2020-04-23 10:21:02 -1000

    Project page now displays a basic table with correct data.

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,25 +1,72 @@
 import React, { useEffect } from 'react';
 import { usePageData } from '../../context/pageData';
-import { Container } from 'semantic-ui-react';
+import { Container, Table } from 'semantic-ui-react';
 import { useAuth } from "../../context/auth";
+import { isEmpty } from "lodash";
 import axios from 'axios';
 
 const ProjectPage = () => {
-  const { mapData, userData, projectData, setProjectData } = usePageData();
+  const { userData, setUserData, projectData, setProjectData } = usePageData();
   const { authTokens } = useAuth();
-
-  console.log(userData)
   useEffect(() => {
-    axios.get("/projects")
-    .then(response => {
-      setProjectData(response);
+    if (isEmpty(userData)){
+      axios.post("/users/dashboard", { data: authTokens })
+      .then(response => {
+        setUserData(response);
+      });
+    }
+    if (userData.data){
+      let bugsArr = userData.data.bugs;
+      let freqObj = {};
+      for (let i of bugsArr){
+        if (!freqObj[i.project.id]){
+          freqObj[i.project.id] = 1;
+          setProjectData(projectData => projectData.concat(i.project));
+        }
+      }
+    }
+  }, [authTokens, userData, setUserData, setProjectData]);
+
+  const mapHeaders = (headers) => {
+    if (!headers) { return false };
+    return headers.map(header => {
+      return (
+        <Table.HeaderCell key={header}>{header}</Table.HeaderCell>
+      )
     })
-  }, [setProjectData])
+  }
+
+  const mapProjects = (projects) => {
+    if (projects.length === 0) { return false };
+    
+    console.log(projects);
+    return projects.map(project => {
+      return (
+        <Table.Row key={project.id}>
+          <Table.Cell>{project.name}</Table.Cell>
+          <Table.Cell>{`${project.project_creator.first_name} ${project.project_creator.last_name}`}</Table.Cell>
+          <Table.Cell>{project.project_status.status}</Table.Cell>
+          <Table.Cell>{project.created_at.split("T")[0]}</Table.Cell>
+          <Table.Cell>{project.due_date === null ? "-" : project.due_date.split("T")[0]}</Table.Cell>
+        </Table.Row>
+      )
+    })
+  }
 
   return (
     <Container>
       <h1>PROJECT PAGE</h1>
-      {/* <div>{mapData(projectData)}</div> */}
+      <Table celled inverted selectable>
+        <Table.Header>
+          <Table.Row key={"header"}>
+            {mapHeaders(["Project Name", "Owner", "Status", "Start Date", "End Date"])}
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {mapProjects(projectData)}
+        </Table.Body>
+      </Table>
     </Container>
   );
 }
