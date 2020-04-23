eea587dc562c3a3c8b33ac1512f04442e35f6d22
Calvin <calmoon808@gmail.com>
2020-04-23 10:16:59 -1000

    Going to organize future actions alphabetically

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,13 @@
 const axios = require("axios");
 
-export const validateEmail = (email) => {
-  const reg = /\S+@\S+\.\S+/;
-  return reg.test(email);
-}
-
 export const getGroupCount = (url, group, data) => {
   return axios.post(`${url}/count`, { group, data })
   .then(response => {
     return response;
   })
+}
+
+export const validateEmail = (email) => {
+  const reg = /\S+@\S+\.\S+/;
+  return reg.test(email);
 }
