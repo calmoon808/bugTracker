30269c89a706f3c04f5f6bd6f05d4ffcdf2fa74e
Calvin <calmoon808@gmail.com>
2020-04-25 04:39:17 -1000

    commented out potentially unecessary code.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -10,14 +10,14 @@ import SignUp from '../components/SignUpComponent';
 import DashboardPage from '../pages/DashboardPage';
 import AdminPage from '../pages/AdminPage/AdminPage';
 import ProjectPage from '../pages/ProjectPage';
-import ProjectDisplay from '../components/ProjectDisplayComponent';
+// import ProjectDisplay from '../components/ProjectDisplayComponent';
 import ProjectDashboardPage from '../pages/ProjectDashboardPage'
 import PrivateRoute from '../decorators/PrivateRoute';
 import TicketPage from '../pages/TicketPage/TicketPage';
 import ProfilePage from '../pages/ProfilePage/ProfilePage';
 import { AuthContext } from "../context/auth";
 import { PageDataContext } from "../context/pageData";
-import TicketDisplay from '../components/TicketDisplayComponent/TicketDisplay';
+// import TicketDisplay from '../components/TicketDisplayComponent/TicketDisplay';
 
 export default function App() {
   const [authTokens, setAuthTokens] = useState(localStorage.getItem('authTokens') || "");
@@ -26,7 +26,7 @@ export default function App() {
   const [projectData, setProjectData] = useState([]);
   const [ticketData, setTicketData] = useState({});
   const [userData, setUserData] = useState({});
-  const [ referrer, setReferrer ] = useState(-1);
+  const [referrer, setReferrer] = useState(-1);
 
   const setTokens = (data) => {
     localStorage.setItem("authTokens", JSON.stringify(data.session.passport.user));
@@ -35,35 +35,35 @@ export default function App() {
     setIsLoggedIn(data.isAuthenticated);
   }
 
-  const mapData = (data) => {
-    if (data.config === undefined) { return false };
-    if (data.config.url === "/projects"){
-      return data.data.map(project => {
-        return (
-          <ProjectDisplay 
-            key={project.id}
-            project_name={project.name}
-            project_creator={`${project.project_creator.first_name} ${project.project_creator.last_name || ''}`}
-            company_name={project.company.name || ''}
-          />
-        )
-      })
-    }
-    if (data.config.url === "/bugs"){
-      return data.data.map(ticket => {
-        return (
-          <TicketDisplay
-            key={ticket.id}
-            description={ticket.bug}
-            project={ticket.project.name}
-            poster={`${ticket.poster.first_name} ${ticket.poster.last_name || ''}`}
-            bug_status={ticket.bug_status.status}
-            bug_priority={ticket.bug_priority.priority}
-          />
-        )
-      })
-    }
-  }
+  // const mapData = (data) => {
+  //   if (data.config === undefined) { return false };
+  //   if (data.config.url === "/projects"){
+  //     return data.data.map(project => {
+  //       return (
+  //         <ProjectDisplay 
+  //           key={project.id}
+  //           project_name={project.name}
+  //           project_creator={`${project.project_creator.first_name} ${project.project_creator.last_name || ''}`}
+  //           company_name={project.company.name || ''}
+  //         />
+  //       )
+  //     })
+  //   }
+  //   if (data.config.url === "/bugs"){
+  //     return data.data.map(ticket => {
+  //       return (
+  //         <TicketDisplay
+  //           key={ticket.id}
+  //           description={ticket.bug}
+  //           project={ticket.project.name}
+  //           poster={`${ticket.poster.first_name} ${ticket.poster.last_name || ''}`}
+  //           bug_status={ticket.bug_status.status}
+  //           bug_priority={ticket.bug_priority.priority}
+  //         />
+  //       )
+  //     })
+  //   }
+  // }
 
   return (
     <AuthContext.Provider value={{
@@ -79,7 +79,7 @@ export default function App() {
           <Container className="Container">
             <Switch>
               <PageDataContext.Provider value={{
-                mapData,
+                // mapData,
                 projectData, setProjectData,
                 ticketData, setTicketData,
                 userData, setUserData,
