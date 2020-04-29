f4f7fd1802549f305d89477ff19165f77457b3b1
Calvin <calmoon808@gmail.com>
2020-04-28 15:30:05 -1000

    added modal support in the bugstable

--- src/components/BugTableComponent/BugTable.jsx
+++ src/components/BugTableComponent/BugTable.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Table } from 'semantic-ui-react';
+import { usePageData } from "../../context/pageData";
+import { Table, Modal, Button, Form, Accordion, Icon, Comment, Header } from 'semantic-ui-react';
+import styles from './BugTable.module.scss';
 
 const BugTableComponent = (props) => {
+  const {showUsers, setShowUsers} = usePageData();
+
+  const handleClick = () => {
+    showUsers === false ? setShowUsers(true) : setShowUsers(false);
+  }
+
+  
   const mapHeaders = () => {
     if (props.headers === undefined) { return false };
     return props.headers.map(header => {
       return (
         <Table.HeaderCell key={header}>{header}</Table.HeaderCell>
+      )
+    })
+  }
+
+  const mapUsers = (userArr) => {
+    console.log(userArr);
+    if (userArr === undefined) { return false }
+    return userArr.map(user => {
+      return (
+        <p key={user.id}>{`${user.first_name} ${user.last_name}`}</p>
       )
     })
   }
@@ -27,12 +46,63 @@ const BugTableComponent = (props) => {
       let dateFormat = timeStampArr[0];
 
       return (
-        <Table.Row key={bug.id}>
-          <Table.Cell>{bug.bug}</Table.Cell>
-          <Table.Cell>{`${bug.poster.first_name} ${bug.poster.last_name}`}</Table.Cell>
-          <Table.Cell>{bug.bug_status.status}</Table.Cell>
-          <Table.Cell>{dateFormat || '-'}</Table.Cell>
-        </Table.Row>
+        <Modal 
+          closeIcon
+          className={styles.bugModal}
+          key={bug.id}
+          centered={true}
+          size={"fullscreen"}
+          onClose={() => setShowUsers(false)}
+          trigger={
+            <Table.Row>
+              <Table.Cell>{bug.bug}</Table.Cell>
+              <Table.Cell>{`${bug.poster.first_name} ${bug.poster.last_name}`}</Table.Cell>
+              <Table.Cell>{bug.bug_status.status}</Table.Cell>
+              <Table.Cell>{dateFormat || '-'}</Table.Cell>
+            </Table.Row>
+          }
+        >
+          <Modal.Header>Bug Ticket</Modal.Header>
+          <Modal.Content>
+            <h2>id: {bug.id}</h2>
+            <h2>Issue: {bug.bug}</h2>
+            <h3>Poster: {`${bug.poster.first_name} ${bug.poster.last_name}`}</h3>
+            <Accordion>
+              <Accordion.Title
+                active={showUsers}
+                onClick={handleClick}
+              >
+                <Icon name='dropdown' />
+                Assigned to:
+              </Accordion.Title>
+              <Accordion.Content
+                active={showUsers}
+              >
+                {mapUsers(bug.users)}
+              </Accordion.Content>
+            </Accordion>
+            <h2>Status: {bug.bug_status.status}</h2>
+            <h2>Priority: {bug.bug_priority.priority}</h2>
+            <Comment.Group>
+              <Header as='h3' dividing>
+                Comments
+              </Header>
+            </Comment.Group>
+            <Form reply>
+              <Form.TextArea />
+              <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+            </Form>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button negative>Cancel</Button>
+            <Button
+              positive
+              icon='checkmark'
+              labelPosition='right'
+              content='Submit'
+            />
+          </Modal.Actions>
+        </Modal>
       )
     })
   }
