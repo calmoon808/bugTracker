8880b88a3d2edcfb437038af9d2bc0aaa099b95e
Calvin <calmoon808@gmail.com>
2020-05-01 20:36:01 -1000

    added functions to initialize correct feed table and poster

--- server/routes/projects/index.js
+++ server/routes/projects/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const projectRouter = express.Router();
-const Project = require("../../database/models/Project")
+const Project = require("../../database/models/Project");
+const client = require("../../../src/getStream")
 
 projectRouter.route("/")
   .get((req, res) => {
@@ -17,7 +18,15 @@ projectRouter.route("/")
     })
   })
 
-projectRouter.post("/dashboard", (req, res) => {
+projectRouter.post("/setCookie", (req, res) => {
+  let projectToken = client.createUserToken(req.body.projectId.toString());
+  res.cookie('activityFeedToken', projectToken);
+  res.json(200)
+})
+
+projectRouter.post("/dashboard", async (req, res) => {
+  let userId = req.body.authTokens.id.toString();
+  let userName = req.body.authTokens.name;
   let projectId = req.body.projectId.id;
   if (typeof projectId === 'string') projectId = JSON.parse(projectId);
   Project.query()
@@ -30,6 +39,25 @@ projectRouter.post("/dashboard", (req, res) => {
   .catch(err => {
     console.log(err);
     res.json(err);
+  });
+  
+  await client.user(`user_${userId}`).getOrCreate({
+    name: userName
   })
+  client.user(`user_${userId}`).update({name: userName})
+  // .then(() => {
+  //   client.user(`user_${userId}`).get()
+  //   .then(user => {
+  //     console.log(user);
+  //   });
+  // })
+
+  const feed = client.feed('projectFeed', projectId.toString());
+
+  // feed.addActivity({
+  //   'actor': client.user(userId).ref(),
+  //   'verb': 'stab',
+  //   'object': 'I love this picture',
+  // })
 })
 module.exports = projectRouter;
