89dcc3f2d2748e1e7888e8c1d92aea37006661ab
Calvin <calmoon808@gmail.com>
2020-05-19 18:25:42 -1000

    added routes for comments

--- server/routes/comments/index.js
+++ server/routes/comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const commentRouter = express.Router();
 const Comment = require("../../database/models/Comment");
+const client = require("../../../src/getStream");
 
 commentRouter.post("/bug", (req, res) => {
   Comment.query()
@@ -13,9 +14,15 @@ commentRouter.post("/bug", (req, res) => {
 })
 
 commentRouter.post("/add", (req, res) => {
+  const feed = client.feed('projectFeed', req.body[1].toString());
   Comment.query()
-  .insert(req.body)
+  .insert(req.body[0])
   .then(() => {
+    feed.addActivity({
+      'actor': client.user(req.body[0].poster_id).ref(),
+      'verb': 'comment',
+      'object': `has commented on bug #${req.body[0].bug_id}`,
+    })
     res.sendStatus(200);
   })
   .catch(err => {
