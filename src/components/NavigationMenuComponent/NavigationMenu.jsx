50647aa87649555c795265f02d85b6ac12dddcbf
Calvin <calmoon808@gmail.com>
2020-05-20 20:34:33 -1000

    nav menu now shows project/bug submit options depending on what page you are on

--- src/components/NavigationMenuComponent/NavigationMenu.jsx
+++ src/components/NavigationMenuComponent/NavigationMenu.jsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 import { Grid, Menu } from 'semantic-ui-react';
 import styles from "./NavigationMenu.module.scss"
 import { useAuth } from "../../context/auth";
+import BugAddModal from '../BugAddModalComponent';
+import { usePageData } from '../../context/pageData';
+import ProjectAddModal from '../ProjectAddModalComponent';
 
 const NavigationMenu = (props) => {
   const { activePage, setActivePage } = useAuth();
+  const { userData, currentProjectData } = usePageData();
 
   const handlePageClick = (e, { name }) => {
     setActivePage(name);
@@ -13,38 +17,38 @@ const NavigationMenu = (props) => {
   }
 
   return (
-    // <div className={styles.NavigationMenu}>
-      <Grid className={styles.Grid}>
-        <Grid.Column className={styles.Column}>
-          <Menu inverted vertical pointing className={styles.Menu}>
-            <Menu.Item
-              name='BugTracker'
-            />
-            <Menu.Item
-              as={Link} to='/'
-              name='/'
-              content='Dashboard'
-              active={activePage === '/'}
-              onClick={handlePageClick}
-            />
-            <Menu.Item 
-              as={Link} to='/projects'
-              name='/projects' 
-              content='My Projects'
-              active={activePage === '/projects'}
-              onClick={handlePageClick}
-            />
-            <Menu.Item
-              as={Link} to='/profile'
-              name='/profile'
-              content='User Profile'
-              active={activePage === 'profile'}
-              onClick={handlePageClick}
-            />
-          </Menu>
-        </Grid.Column>
-      </Grid>
-    // </div>
+    <Grid className={styles.Grid}>
+      <Grid.Column className={styles.Column}>
+        <Menu inverted vertical pointing className={styles.Menu}>
+          <Menu.Item
+            name='BugTracker'
+          />
+          <Menu.Item
+            as={Link} to='/'
+            name='/'
+            content='Dashboard'
+            active={activePage === '/'}
+            onClick={handlePageClick}
+          />
+          <Menu.Item 
+            as={Link} to='/projects'
+            name='/projects' 
+            content='My Projects'
+            active={activePage === '/projects'}
+            onClick={handlePageClick}
+          />
+          <Menu.Item
+            as={Link} to='/profile'
+            name='/profile'
+            content='User Profile'
+            active={activePage === 'profile'}
+            onClick={handlePageClick}
+          />
+          {(userData && window.location.pathname === "/") && <ProjectAddModal />}
+          {(currentProjectData && window.location.pathname.includes("/projects/")) && <BugAddModal />}
+        </Menu>
+      </Grid.Column>
+    </Grid>
   );
 }
 
